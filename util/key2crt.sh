ac58c09ae62a2ca598dfa41f009d6a76890f1c0d
Renato Figueiro Maia <maia@tecgraf.puc-rio.br>
2014-03-17 00:49:15 +0000

    Correcoes nos scripts de geracao de chave.
    git-svn-id: https://subversion.tecgraf.puc-rio.br/engdist/openbus/treinamentos/fontes/curso-tecgraf@150742 ae0415b3-e90b-0410-900d-d0be9363c56b

--- util/key2crt.sh
+++ util/key2crt.sh
@@ -1,3 +1,4 @@
 set -e
 
-openssl req -new -x509 -key $1 -keyform DER  -out $2 -outform DER
+openssl req -new -x509 -key $1 -keyform DER -out $2 -outform DER
+
--- util/newkey.sh
+++ util/newkey.sh
@@ -1,5 +1,5 @@
 set -e
 
-openssl genrsa -out /tmp/openbus-utils-new.key 2048
+openssl genrsa -out /tmp/openbus-temp.key 2048
 openssl pkcs8 -topk8 -nocrypt -in /tmp/openbus-temp.key -out $1 -outform DER
 rm -f /tmp/openbus-temp.key
